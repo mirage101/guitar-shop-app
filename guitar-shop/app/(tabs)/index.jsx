b96a246df6a830a805a1ae5e03881bab1e1d9e30
Ivan Filipov <ivan@stanga.net>
2026-02-25 14:40:09 +0200

    added product cart component

--- guitar-shop/app/(tabs)/index.jsx
+++ guitar-shop/app/(tabs)/index.jsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Constants from "expo-constants";
+import ProductCard from '../../components/productCard';
+
+const BASE_URL = Constants.expoConfig.extra.BASE_URL;
 export default function App() {
+  const [products, setProducts] = useState([]);
+  const fetchProductData = () =>{
+    fetch(`${BASE_URL}/api/products`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Request failed with status ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => setProducts(data?.data ?? []))
+      .catch((error) => console.log(error));
+  }
+
+  
+  useEffect(()=>{
+    fetchProductData()
+    console.log(products);
+  }, []);
   return (
     <SafeAreaView className="min-h-screen">
-      <View className="bg-white flex-1">
-        <Text>test</Text>     
+      <View className="flex-1 bg-white">
+        <ProductCard product={products[0]}/>   
       </View>
     </SafeAreaView>
   );
---
+++ guitar-shop/components/productCard.jsx
@@ -0,0 +1,12 @@
+import {Text, View} from "react-native";
+
+const ProductCard = ({product}) => {
+    console.log(product);
+    return (
+        <View>
+            <Text>Product Card</Text>
+        </View>
+    )
+}
+
+export default ProductCard;
